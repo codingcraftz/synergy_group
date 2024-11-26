dfc01abc44b62b7ffb99fcd97d9449b14c68cbfb
zoonyoung <9851248@gmail.com>
2024-11-26 14:26:39 +0900

    refactor: Modify config.mjs파일 수정(no-cache) for metadata picture

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // 모든 경로에 대해 캐시 무효화
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate", // 캐시를 완전히 방지
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
